e91698550a0da837756721b38b3cc67fd697aaaa
JordanEspitalier <72070967+JordanEspitalier@users.noreply.github.com>
2024-03-16 13:26:57 +0100

    remore useless code in app and login component

--- src/App.tsx
+++ src/App.tsx
@@ -2,63 +2,31 @@
 import { useEffect } from 'react'
 import './App.css'
 import Home from './pages/Home'
-import Login from './pages/Login'
 import { useUserStore } from './store/user'
-import { useAuthStore } from './store/auth'
 import { currentToken, getToken } from './services/auth'
 import { getCurrentUserProfile } from './services/apiRequest/user'
-import { Outlet, useNavigate } from 'react-router-dom'
 
 function App() {
 
-  const userIsLogged = useAuthStore(state => state.userIsLogged)
-  const setUserIsLogged = useAuthStore(state => state.setUserIsLogged)
   const setCurrentUserProfile = useUserStore(state => state.setCurrentUserProfile)
-  const navigate = useNavigate()
 
   useEffect(()=>
   {
-    // On page load, try to fetch auth code from current browser search URL
-    const args = new URLSearchParams(window.location.search)
-    const code = args.get('code')
 
-    // If we find a code, we're in a callback, do a token exchange
-    if(code)
-    {
-      getToken(code).then(()=>setUserIsLogged(true))
-
-      // Remove code from URL so we can refresh correctly.
-      const url = new URL(window.location.href)
-      url.searchParams.delete('code')
-      const updatedUrl = url.search ? url.href : url.href.replace('?', '');
-      window.history.replaceState({}, document.title, updatedUrl);
-
-    }
     // If we have a token, we're logged in
     if(currentToken.access_token && currentToken.access_token!= 'undefined')
     {
-      setUserIsLogged(true)
       // Get user from api and store it
       getCurrentUserProfile().then(user => {
-        //console.log('looged')
         setCurrentUserProfile(user)
-        //navigate('/')
       })
-
-    }
-    // Otherwise we're not logged in
-    if (!currentToken.access_token || currentToken.access_token === 'undefined') {
-      console.log(currentToken.access_token)
-      setUserIsLogged(false)
-      //navigate('/login')
-
     }
 
-  },[userIsLogged])
+
+  },[])
   return (
     <>
       <Home />
-      
     </>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,59 +1,11 @@
 
-import { useEffect } from "react"
-import { currentToken, getToken, redirectToSpotifyAuthorize } from "../services/auth"
-import { useNavigate } from "react-router-dom"
-import { useAuthStore } from "../store/auth"
-import { useUserStore } from "../store/user"
-import { getCurrentUserProfile } from "../services/apiRequest/user"
+
+import { redirectToSpotifyAuthorize } from "../services/auth"
+
 
 export default function Login () 
 {
     const loginWithSpotifyClick = async () => await redirectToSpotifyAuthorize()
-
-    const userIsLogged = useAuthStore(state => state.userIsLogged)
-    const setUserIsLogged = useAuthStore(state => state.setUserIsLogged)
-    const setCurrentUserProfile = useUserStore(state => state.setCurrentUserProfile)
-    const navigate = useNavigate()
-  
-    useEffect(()=>
-    {
-      // On page load, try to fetch auth code from current browser search URL
-      const args = new URLSearchParams(window.location.search)
-      const code = args.get('code')
-  
-      // If we find a code, we're in a callback, do a token exchange
-      if(code)
-      {
-        getToken(code).then(()=>setUserIsLogged(true))
-  
-        // Remove code from URL so we can refresh correctly.
-        const url = new URL(window.location.href)
-        url.searchParams.delete('code')
-        const updatedUrl = url.search ? url.href : url.href.replace('?', '');
-        window.history.replaceState({}, document.title, updatedUrl);
-  
-      }
-      // If we have a token, we're logged in
-      if(currentToken.access_token && currentToken.access_token!= 'undefined')
-      {
-        setUserIsLogged(true)
-        // Get user from api and store it
-        getCurrentUserProfile().then(user => {
-          //console.log('looged')
-          setCurrentUserProfile(user)
-          //navigate('/')
-        })
-  
-      }
-      // Otherwise we're not logged in
-      if (!currentToken.access_token || currentToken.access_token === 'undefined') {
-        console.log(currentToken.access_token)
-        setUserIsLogged(false)
-        //navigate('/login')
-  
-      }
-  
-    },[userIsLogged])
 
     return (
         <div className="login">
