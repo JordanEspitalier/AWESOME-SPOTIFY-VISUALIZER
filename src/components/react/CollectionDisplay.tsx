e66efba4933b7af433f72586c76bd0d6626e2185
JordanEspitalier <72070967+JordanEspitalier@users.noreply.github.com>
2024-03-26 15:11:14 +0100

    finish likedTracks library update

--- src/components/react/CollectionDisplay.tsx
+++ src/components/react/CollectionDisplay.tsx
@@ -7,39 +7,67 @@ import './collectionDisplay.css'
 
 function CollectionDisplay({type}:{type : string}) {
 const {query} = useParams()
-console.log(query)
 
+const [nextQuery, setNextQuery] = useState<string>()
+const [totalItems, setTotalItems] = useState<number>()
 const [items, setItems] = useState<any>()
-console.log(items)
 
 useEffect(()=>
 {
     if(type === 'likedTracks')
     {
-        getCurrentUserSavedTracks({limit : 50})
-        .then(data => setItems(data.items))
-        .catch(error => console.log(error))
+        if(nextQuery)
+        {
+            getCurrentUserSavedTracks({limit : 50, next : nextQuery})
+            .then(data =>{
+                const newItems = [...items]
+                data.items.map((item : any) => newItems.push(item))
+                setItems(newItems)
+                setTotalItems(data.total)
+                if(data.next) setNextQuery(data.next)
+            })
+            .catch(error => console.log(error))
+        }
+        else
+        {
+            getCurrentUserSavedTracks({limit : 50})
+            .then(data =>{
+                setItems(data.items)
+                setTotalItems(data.total)
+                if(data.next) setNextQuery(data.next)
+            })
+            .catch(error => console.log(error))
+        }
     }
-},[])
+},[nextQuery])
   return (
     <div className="collectionDisplay">
     {items &&
       <>
         <header className="collectionDisplay-header">
+        {type === 'likedTracks' &&
+            <>
+            <img className="collectionDisplay-header-image" src="https://misc.scdn.co/liked-songs/liked-songs-640.png"/>
+            <div className="collectionDisplay-header-infos">
+                <div className="collectionDisplay-header-infos-title">Liked tracks</div>
+                <div className="collectionDisplay-header-infos-totalItems">{totalItems} tracks</div>
+            </div>
+            </>
+        }
         </header> 
-       <table className='collectionDisplay-table'>
-        <thead className='collectionDisplay-table-header'>
-          <tr>
-              <th scope="col">#</th>
-              <th scope="col">Title</th>
-              <th scope="col">Album</th>
-              <th scope="col">Time</th>
-          </tr>
-        </thead>
-        <tbody className='collectionDisplay-table-body'>
-          {items.map((item : any, index:any) => <TrackRow key={index} track={item.track} tableIndex={index + 1}/>)}
-        </tbody>
-       </table>
+        <table className='collectionDisplay-table'>
+            <thead className='collectionDisplay-table-header'>
+                <tr className='collectionDisplay-table-header-tr'>
+                    <th scope="col" className='collectionDisplay-table-header-tr-hash'>#</th>
+                    <th scope="col" className='collectionDisplay-table-header-tr-title'>Title</th>
+                    <th scope="col" className='collectionDisplay-table-header-tr-album'>Album</th>
+                    <th scope="col" className='collectionDisplay-table-header-tr-time'>Time</th>
+                </tr>
+            </thead>
+            <tbody className='collectionDisplay-table-body'>
+            {items.map((item : any, index:any) => <TrackRow key={index} track={item.track} tableIndex={index + 1}/>)}
+            </tbody>
+        </table>
       </>
       }
     </div>
